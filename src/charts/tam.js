db04b0e8f0278240a51c190fdfe1c22728105ba7
David Rosson <david@rosson.com.au>
2019-06-14 12:37:18 +0200

    Add stacked area chart

--- src/charts/tam.js
+++ src/charts/tam.js
@@ -4,7 +4,16 @@ import transform from '../utils/transformData'
 const columns = transform(rawData);
 
 const params = {
-  columns: columns
+  columns: columns,
+  types: {
+    'total': 'spline',
+    'bot_only': 'area-spline',
+    'bot_agent': 'area-spline',
+    'agent_only': 'area-spline'
+  },
+  groups: [
+    ['bot_only', 'bot_agent', 'agent_only']
+  ]
 };
 
 export default params;
--- src/utils/transformData.js
+++ src/utils/transformData.js
@@ -1,9 +1,23 @@
+const FIELDS = ['total', 'bot_only', 'bot_agent', 'agent_only'];
+
 const transformData = (rawData) => {
-  var transformed = [
-    ['data1', 30, 200, 100, 400, 150, 250],
-    ['data2', 50, 20, 10, 40, 15, 25]
-  ]
-  return transformed;
+  // Get the number of days
+  const size = rawData.length;
+  const columns = {};
+  // Initialise arrays for daily values
+  FIELDS.forEach(field => {
+    columns[field] = Array(size);
+  });
+  // Map daily values to separate vectors
+  rawData.forEach((day, index) => {
+    FIELDS.forEach(field => {
+      columns[field][index] = day[field] || 0;
+    })
+  });
+  // Return an array of columns with headers
+  return FIELDS.map(field => {
+    return [ field ].concat(columns[field])
+  })
 };
 
 export default transformData;
