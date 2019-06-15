63c46f818017e22ea15b15ca496459d01081407e
David Rosson <david@rosson.com.au>
2019-06-15 19:26:55 +0200

    Add Lodash notes

--- src/utils/transformData.js
+++ src/utils/transformData.js
@@ -1,9 +1,17 @@
 const FIELDS = ['date', 'total', 'bot_only', 'bot_agent', 'agent_only'];
 
 const transformData = (rawData) => {
+  // Option 1: Lodash transpose
+  // const headers = Object.keys(rawData[0]);
+  // const dataRows = rawData.map(day => _.values(day));
+  // return _.zip(headers, ...dataRows);
+
+  // Option 2: Map each key
   // return Object.keys(rawData[0]).map(columnKey => {
   //   return [columnKey].concat(rawData.map(d => d[columnKey]));
-  // })
+  // });
+
+  // Option 3: 1 x n
   // Get the number of days
   const size = rawData.length;
   const columns = {};
@@ -18,8 +26,8 @@ const transformData = (rawData) => {
     })
   });
   // Return an array of columns with headers
-  return FIELDS.map(field => {
-    return [ field ].concat(columns[field])
+  return FIELDS.map(header => {
+    return [ header, ...columns[header] ]
   })
 };
 
